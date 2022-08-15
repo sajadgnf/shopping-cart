2b37fa61b06bb09f751b97843a9aa3f50baa2311
Sajad Khazraee Ganjifar <sajadganjifar@gmail.com>
2022-08-15 13:06:30 +0430

    fix the footer stick to bottom problem

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 
+// styles
+import styles from "./Layout.module.scss"
+
 const Layout = ({ children }) => {
     return (
-        <>
+        <div className={styles.container}>
             <Header />
             {children}
             <Footer />
-        </>
+        </div>
     );
 };
 
